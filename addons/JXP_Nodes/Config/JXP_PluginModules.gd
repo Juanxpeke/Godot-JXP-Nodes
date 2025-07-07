07a07b33e9bc547aa109a66ee771c2b78a0d518b
Juanxpeke <juanintendo.f11@gmail.com>
2025-07-06 20:31:26 -0400

    Module installation logic

--- addons/JXP_Nodes/Config/JXP_PluginModules.gd
+++ addons/JXP_Nodes/Config/JXP_PluginModules.gd
@@ -1,7 +1,5 @@
 class_name JXP_PluginModules extends Object
 ## Object that stores all the plugin modules information.
-##
-## This object is designed to be passed to a [EditorInspector].
 #region Signals
 #endregion Signals
 
@@ -20,18 +18,21 @@ var _modules := {
 		"name": "Outlines",
 		"category": "Rendering 3D",
 		"description": "Outlines for 3D elements and related scripts",
-		"installed": true,
+		"plugin_relative_path": "Graphics/Outlines/",
+		"installed": false,
 	},
 	"hittables": {
 		"name": "Hittables",
 		"category": "Physics 3D",
 		"description": "Hittables",
+		"plugin_relative_path": "Physics/Hittables/",
 		"installed": false,
 	},
 	"methods_list": {
 		"name": "Methods List",
 		"category": "Debug",
 		"description": "A new inspector tab that allows the call of methods of any object in run-time",
+		"plugin_relative_path": "Debug/MethodsList/",
 		"installed": false,
 	},
 }
@@ -39,21 +40,26 @@ var _modules := {
 
 #region Built-in Virtual Methods
 func _init() -> void:
-	pass
+	check_installed_state()
 #endregion Built-in Virtual Methods
 
 #region Public Methods
+## TODO.
 func get_modules() -> Array:
 	return _modules.values()
+## TODO.
+func check_installed_state() -> void:
+	for module_name in _modules:
+		var module : Dictionary = _modules[module_name]
+		var module_path : String = module.plugin_relative_path
+		
+		var dir := DirAccess.open("res://addons/JXP_Nodes/" + module_path)
+		if dir == null:
+			module.installed = false
+		else:
+			module.installed = true
+		_modules[module_name] = module
 #endregion Public Methods
 
 #region Private Methods
-func _update_project_modules() -> void:
-	for module_name : StringName in _modules.keys():
-		var module : Dictionary = _modules[module_name]
-		
-		if EditorInterface.get_editor_settings().get_project_metadata("jxp_plugin_settings", module_name, null) == null:
-			EditorInterface.get_editor_settings().set_project_metadata("jxp_plugin_settings", module_name, _modules[module_name].cache)
-		else:
-			pass
 #endregion Private Methods
--- addons/JXP_Nodes/Editor/JXP_EditorInstaller.gd
+++ addons/JXP_Nodes/Editor/JXP_EditorInstaller.gd
@@ -13,6 +13,8 @@ enum _ModuleButtonIndex {
 #endregion Enums
 
 #region Constants
+const _PLUGIN_RELEASES_URL := "https://api.github.com/repos/Juanxpeke/Godot-JXP-Nodes/releases"
+const _TEMP_FILE_NAME := "user://jxp_nodes_temp.zip"
 #endregion Constants
 
 #region Exports Variables
@@ -24,8 +26,18 @@ enum _ModuleButtonIndex {
 #region Private Variables
 # Data
 var _plugin_modules : JXP_PluginModules = null
+var _current_version : _ParsedVersion = null
+var _current_version_info : Dictionary = {}
+var _latest_version_info : Dictionary = {} # NOTE: We won't use https://api.github.com/repos/Juanxpeke/Godot-JXP-Nodes/releases/latest
 # Nodes
+var _releases_request : HTTPRequest = null
+var _current_version_request : HTTPRequest = null
+
+var _plugin_version : Label = null
 var _modules_tree : Tree = null
+var _selected_module_container : PanelContainer = null
+var _selected_module_title : Label = null
+var _selected_module_description : Label = null
 #endregion Private Variables
 
 #region On Ready Variables
@@ -35,23 +47,71 @@ var _modules_tree : Tree = null
 func _init() -> void:
 	_plugin_modules = JXP_PluginModules.new()
 	
+	_current_version = _get_current_version()
+	
+	if not _current_version.valid:
+		pass # TODO: UI and UX
+	
 	add_theme_stylebox_override("panel", _get_panel_style_box())
+	
+	_releases_request = HTTPRequest.new()
+	_releases_request.request_completed.connect(_on_releases_request_completed)
+	add_child(_releases_request)
+	
+	_current_version_request = HTTPRequest.new()
+	_current_version_request.request_completed.connect(_on_current_version_request_completed)
+	add_child(_current_version_request)
 	
 	var main_vb = VBoxContainer.new()
 	main_vb.alignment = BoxContainer.ALIGNMENT_BEGIN
 	main_vb.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	main_vb.add_theme_constant_override("separation", 0)
 	add_child(main_vb)
+	
+	var top_hb := HBoxContainer.new()
+	main_vb.add_child(top_hb)
+	
+	_plugin_version = Label.new()
+	_plugin_version.text = "Requesting plugin version..."
+	_plugin_version.clip_text = true
+	_plugin_version.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	top_hb.add_child(_plugin_version)
+	
+	var bottom_hb := HBoxContainer.new()
+	bottom_hb.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	main_vb.add_child(bottom_hb)
 	
 	_modules_tree = Tree.new()
 	_modules_tree.hide_root = true
-	_modules_tree.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	_modules_tree.custom_minimum_size = Vector2(300, 0) * EditorInterface.get_editor_scale()
 	_modules_tree.button_clicked.connect(_on_modules_tree_button_clicked)
-	main_vb.add_child(_modules_tree)
+	bottom_hb.add_child(_modules_tree)
+	
+	_selected_module_container = PanelContainer.new()
+	_selected_module_container.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	_selected_module_container.add_theme_stylebox_override("panel", EditorInterface.get_editor_theme().get_stylebox("panel", "Tree"))
+	bottom_hb.add_child(_selected_module_container)
+	
+	var selected_module_vb := VBoxContainer.new()
+	_selected_module_container.add_child(selected_module_vb)
+	
+	_selected_module_title = Label.new()
+	_selected_module_title.text = "Module"
+	_selected_module_title.add_theme_font_override("font", EditorInterface.get_editor_theme().get_font("title", "EditorFonts"))
+	selected_module_vb.add_child(_selected_module_title)
 	
 	_update_modules_tree()
 
+func _ready() -> void:
+	# HTTP nodes have to be inside tree to make requests
+	_releases_request.request(_PLUGIN_RELEASES_URL)
+
+func _exit_tree() -> void:
+	DirAccess.remove_absolute(_TEMP_FILE_NAME)
+
 func _notification(what : int) -> void:
 	if what == NOTIFICATION_THEME_CHANGED:
+		_selected_module_container.add_theme_stylebox_override("panel", EditorInterface.get_editor_theme().get_stylebox("panel", "Tree"))
 		_update_modules_tree()
 #endregion Built-in Virtual Methods
 
@@ -60,13 +120,86 @@ func _notification(what : int) -> void:
 
 #region Private Methods
 #region Callbacks
+func _on_releases_request_completed(result : int, response_code : int, headers : PackedStringArray, body : PackedByteArray) -> void:
+	if result != HTTPRequest.RESULT_SUCCESS:
+		# TODO: UI and UX
+		return
+	
+	# The response should be an array of dictionaries (each for the information of each release)
+	var response : Variant = JSON.parse_string(body.get_string_from_utf8())
+	if typeof(response) != TYPE_ARRAY:
+		# TODO: UI and UX
+		return
+	
+	var releases : Array = response
+	
+	for release : Dictionary in releases:
+		var release_version := _ParsedVersion.from_string(release.tag_name)
+		if _current_version.is_equal(release_version):
+			_current_version_info = release
+			_current_version_request.request(_current_version_info.zipball_url)
+	
+	# UI and UX stuff
+	_plugin_version.text = "Version " + _current_version_info.tag_name.trim_prefix('v')
+
+func _on_current_version_request_completed(result : int, response_code : int, headers : PackedStringArray, body : PackedByteArray) -> void:
+	if result != HTTPRequest.RESULT_SUCCESS:
+		# TODO: UI and UX
+		return
+	
+	# Save the downloaded ZIP as a temporal file, this ZIP file will be available as long
+	# as the modules installer interface is visible
+	var zip_file := FileAccess.open(_TEMP_FILE_NAME, FileAccess.WRITE)
+	zip_file.store_buffer(body)
+	zip_file.close()
+
 func _on_modules_tree_button_clicked(item : TreeItem, column : int, id : int, mouse_button_index : int) -> void:
+	var module : Dictionary = item.get_metadata(0)
 	match id:
 		_ModuleButtonIndex.INSTALL:
-			pass
+			_install_module(module.plugin_relative_path)
 		_ModuleButtonIndex.UNINSTALL:
-			pass
+			_uninstall_module(module.plugin_relative_path)
 #endregion Callbacks
+
+func _get_current_version() -> _ParsedVersion:
+	var plugin_cfg := ConfigFile.new()
+	plugin_cfg.load("res://addons/JXP_Nodes/plugin.cfg")
+	var version : String = plugin_cfg.get_value("plugin", "version", "unknown version")
+	return _ParsedVersion.from_string(version)
+
+func _install_module(module_path : String) -> void:
+	var zip_reader := ZIPReader.new()
+	zip_reader.open(_TEMP_FILE_NAME)
+	
+	var release_files_paths : PackedStringArray = zip_reader.get_files()
+	var release_folder_name : String = release_files_paths[0]
+	var release_plugin_path: String = release_folder_name.path_join('addons/JXP_Nodes/')
+	
+	for release_file_path in release_files_paths:
+		if not release_plugin_path + module_path in release_file_path:
+			continue
+
+		var relative_file_path: String = release_file_path.replace(release_plugin_path, "")
+		if release_file_path.ends_with("/"):
+			DirAccess.make_dir_recursive_absolute("res://addons/JXP_Nodes/".path_join(relative_file_path))
+		else:
+			var file: FileAccess = FileAccess.open("res://addons/JXP_Nodes/".path_join(relative_file_path), FileAccess.WRITE)
+			file.store_buffer(zip_reader.read_file(release_file_path))
+	
+	zip_reader.close()
+	
+	_plugin_modules.check_installed_state()
+	_update_modules_tree()
+	#DirAccess.remove_absolute(_TEMP_FILE_NAME)
+
+func _uninstall_module(module_path : String) -> void:
+	var error := OS.move_to_trash(ProjectSettings.globalize_path("res://addons/JXP_Nodes/" + module_path))
+	# TODO: UI and UX
+	_plugin_modules.check_installed_state()
+	_update_modules_tree()
+	
+
 func _get_panel_style_box() -> StyleBox:
 	# TODO: Godot Engine defines this as a global style box, that is used in multiple instances
 	var panel_style_box := StyleBoxEmpty.new()
@@ -85,6 +218,7 @@ func _update_modules_tree() -> void:
 	
 	var installed_icon := EditorInterface.get_editor_theme().get_icon("ImportCheck", "EditorIcons")
 	var uninstalled_icon := EditorInterface.get_editor_theme().get_icon("ImportFail", "EditorIcons")
+	var reinstall_icon := EditorInterface.get_editor_theme().get_icon("Reload", "EditorIcons")
 	var install_icon := EditorInterface.get_editor_theme().get_icon("AssetLib", "EditorIcons")
 	var uninstall_icon := EditorInterface.get_editor_theme().get_icon("Remove", "EditorIcons")
 	
@@ -101,12 +235,65 @@ func _update_modules_tree() -> void:
 		var module_item := _modules_tree.create_item(category_item)
 		module_item.set_text(0, module.name)
 		module_item.set_tooltip_text(0, module.description)
-		module_item.add_button(0, install_icon, _ModuleButtonIndex.INSTALL, module.installed, "Install")
-		module_item.add_button(0, uninstall_icon, _ModuleButtonIndex.UNINSTALL, not module.installed, "Uninstall")
+		module_item.set_metadata(0, module)
 		
 		if module.installed:
 			module_item.set_icon(0, installed_icon)
+			module_item.add_button(0, reinstall_icon, _ModuleButtonIndex.INSTALL, false, "Reinstall")
+			module_item.add_button(0, uninstall_icon, _ModuleButtonIndex.UNINSTALL, false, "Uninstall")
 		else:
 			module_item.set_icon(0, uninstalled_icon)
-		
+			module_item.add_button(0, install_icon, _ModuleButtonIndex.INSTALL, false, "Install")
+			module_item.add_button(0, uninstall_icon, _ModuleButtonIndex.UNINSTALL, true, "Uninstall")
 #endregion Private Methods
+
+#region Private Inner Classes
+class _ParsedVersion:
+	# TODO: Prerelease and metadata variables and logic
+	var major : int = 0
+	var minor : int = 0
+	var patch : int = 0
+	var valid : bool = false
+	
+	static func from_string(version : String) -> _ParsedVersion:
+		var parsed_version := _ParsedVersion.new()
+		
+		version = version.strip_edges().trim_prefix('v')
+		version = version.substr(0, version.find('('))
+		version = version.to_lower()
+
+		var regex := RegEx.create_from_string(r"^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$")
+		var result : RegExMatch = regex.search(version)
+		if result == null:
+			parsed_version.valid = false
+			return parsed_version
+		else:
+			parsed_version.valid = true
+		
+		if result.get_string("major") != "":
+			parsed_version.major = int(result.get_string("major"))
+		if result.get_string("minor") != "":
+			parsed_version.minor = int(result.get_string("minor"))
+		if result.get_string("patch") != "":
+			parsed_version.patch = int(result.get_string("patch"))
+		
+		return parsed_version
+	
+	func is_equal(parsed_version : _ParsedVersion) -> bool:
+		if major != parsed_version.major:
+			return false
+		if minor != parsed_version.minor:
+			return false
+		if patch != parsed_version.patch:
+			return false
+		return true
+	
+	func is_greater_than(parsed_version : _ParsedVersion) -> bool:
+		if major > parsed_version.major:
+			return true
+		if minor > parsed_version.minor:
+			return true
+		if patch > parsed_version.patch:
+			return true
+		return false
+#endregion Private Inner Classes
